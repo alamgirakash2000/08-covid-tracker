f64382c86e5439546d7d02401305b0614be0344c
Md. Alamgir Hossain <alamgirakash2000@gmail.com>
2020-08-19 23:04:45 +0600

    with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import React, { useEffect } from "react";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./Home";
 import VaccinePage from "./VaccinePage.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Redirect exact from="/" to="/home" />
-      <Switch>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route path="/vaccine">
-          <VaccinePage />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/vaccine" component={VaccinePage}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
